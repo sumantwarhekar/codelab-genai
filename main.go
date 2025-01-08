f097cf736df8f8f137bb9b8618c3fef150b0686c
Sumant Warhekar <sumantwarhekar@gmail.com>
2025-01-08 00:50:46 +0000

    add latest changes

--- main.go
+++ main.go
@@ -1,15 +1,71 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"cloud.google.com/go/compute/metadata"
+	"github.com/firebase/genkit/go/plugins/dotprompt"
+	"github.com/firebase/genkit/go/plugins/vertexai"
 )
 
 func main() {
+	ctx := context.Background()
+	var projectId string
+	var err error
+	projectId = os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if projectId == "" {
+		projectId, err = metadata.ProjectIDWithContext(ctx)
+		os.Setenv("GOOGLE_CLOUD_PROJECT", projectId)
+		if err != nil {
+			return
+		}
+	}
+
+	if err := vertexai.Init(ctx, nil); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	dotprompt.SetDirectory("./")
+	prompt, err := dotprompt.Open("animal-facts")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	type AnimalPromptInput struct {
+		Animal string `json:"animal"`
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, world!")
+
+		animal := r.URL.Query().Get("animal")
+		if animal == "" {
+			animal = "dog"
+		}
+
+		response, err := prompt.Generate(
+			ctx,
+			&dotprompt.PromptRequest{
+				Variables: AnimalPromptInput{
+					Animal: animal,
+				},
+			},
+			nil,
+		)
+
+		if err != nil {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			fmt.Fprint(w, err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		fmt.Fprint(w, response.Text())
+
 	})
 
 	port := os.Getenv("PORT")
